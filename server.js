198a125108439eab065959ab5764709da32ce4fb
lokeshvinodgulhane <109209411+lokeshvinodgulhane@users.noreply.github.com>
2022-07-13 14:18:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -109,6 +109,12 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/hello?name=/', function(request, response) {
+    // userId is a parameter in the url request
+    response.writeHead(200); // return 200 HTTP OK status
+    response.end('{ name:' + request.route.query.name+'}');
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
